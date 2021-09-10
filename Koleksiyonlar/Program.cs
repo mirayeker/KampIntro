3bdef056ae48bb67bbe2068a2346635cd61e6fb9
Miray <mirayekerr@gmail.com>
2021-09-10 12:11:17 +0300

    9 Eylül

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Ayşe", "Miray", "Simge", "Enes" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //ctrl+k+ctr+c -> ctrl+kctrl+u (toplu comment alıp almama
+
+            List<string> isimler2 = new List<string>() { "Murat", "Halil", "Kerem" };
+            isimler2.Add("Engin");
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            
+        
+        }
+    }
+}
